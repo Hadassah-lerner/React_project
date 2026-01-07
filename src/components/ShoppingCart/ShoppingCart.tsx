efc280f46db15de2a973070bf0168bc9aad377e3
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-07 19:58:53 +0200

    Refactor ShoppingCart component for clarity

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,56 +1,55 @@
 import React, { FC } from 'react';
 import './ShoppingCart.scss';
-import './ShoppingCart.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { ProductModel } from '../../models/ProductModel';
 import { deleteProduct } from '../../redux/slices/shoppingCartSlice';
 import { setMessage } from '../../redux/slices/systemMessageSlice';
+import { RootState } from '../../redux/store';
 
-const ShoppingCart: React.FC = () => {
-const cartItems: ProductModel[] = useSelector((state: any) => state.shoppingCart.productList)
-
-const dispatch = useDispatch();
+const ShoppingCart: FC = () => {
+  const dispatch = useDispatch();
+  const cartItems: ProductModel[] = useSelector(
+    (state: RootState) => state.shoppingCart.productList
+  );
 
   const deleteFromCart = (product: ProductModel) => {
     dispatch(deleteProduct(product));
-    dispatch(setMessage("המוצר נמחק מהעגלה"));
-
+    dispatch(setMessage(`המוצר "${product.name}" נמחק מהעגלה`));
   };
 
- return (
-  <div className="ShoppingCart">
+  return (
+    <div className="ShoppingCart">
+      <h2>עגלת קניות ({cartItems.length})</h2>
 
-    <h2>עגלת קניות</h2>
+      {cartItems.length === 0 ? (
+        <p>העגלה ריקה</p>
+      ) : (
+        <ul className="cart-items">
+          {cartItems.map((item) => (
+            <li className="cart-card" key={item.id}>
+              <img
+                src={item.image}
+                alt={item.name}
+                className="cart-thumbnail"
+              />
+              <div className="cart-info">
+                <div className="cart-title">{item.name}</div>
+                <div className="cart-cost">₪{item.price}</div>
+              </div>
 
-    {cartItems.length == 0 ? (
-      <p>העגלה ריקה</p>
-    ) : (
-<ul className="cart-items">
-  {cartItems.map((item) => (
-    <li className="cart-card" key={item.id}>
-      <img src={item.image} alt={item.name} className="cart-thumbnail" />
-      
-      <div className="cart-info">
-        <div className="cart-title">{item.name}</div>
-        <div className="cart-cost">₪{item.price}</div>
-      </div>
-
-      <button
-        className="cart-remove-btn"
-        onClick={() => deleteFromCart(item)}
-      >
-        X
-      </button>
-    </li>
-  ))}
-</ul>
-
-    )}
-  </div>
-);
-}
+              <button
+                className="cart-remove-btn"
+                onClick={() => deleteFromCart(item)}
+                aria-label={`הסר ${item.name} מהעגלה`}
+              >
+                ✕
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default ShoppingCart;
-
-
-
