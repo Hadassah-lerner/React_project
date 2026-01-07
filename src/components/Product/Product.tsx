cad0af2805af2bfb58cd71cc424505cb680e53a4
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-07 17:13:17 +0200

    Fix import path for API functions in Product.tsx

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getProductById, updateProduct } from '../../api/apis';
+import { getProductById, updateProduct } from '../../apis/apis';;
 import './Product.scss';
 
 interface Product {
