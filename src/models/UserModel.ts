9af6894de32783d4bf3495ed44831fb0c9042adf
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-07 20:45:04 +0200

    Update UserModel.ts

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -3,7 +3,7 @@ export class UserModel {
        name!:string
        email!:string
        password!:string
-       role!:string 
+       role: userFromApi.role ?? 'customer',
 
   constructor() {
   this.id = ''
