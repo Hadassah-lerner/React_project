6c30efda57177f5f5838fec90a23f9f664533e3c
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-07 19:10:07 +0200

    Refactor store setup and fix product slice key

--- src/index.tsx
+++ src/index.tsx
@@ -5,33 +5,31 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { shoppingCartSlice } from './redux/slices/shoppingCartSlice';
 import { userSlice } from './redux/slices/userSlice';
 import { productSlice } from './redux/slices/productSlice';
 import { systemMessageSlice } from './redux/slices/systemMessageSlice';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-const mystore = configureStore({
+const store = configureStore({
   reducer: {
     shoppingCart: shoppingCartSlice.reducer,
     user: userSlice.reducer,
-    prouct: productSlice.reducer,
-    systemMessage:systemMessageSlice.reducer,
+    product: productSlice.reducer, // תקן כאן
+    systemMessage: systemMessageSlice.reducer,
   },
 });
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
-  <Provider store={mystore}>
-  <BrowserRouter>
-    <App></App>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
