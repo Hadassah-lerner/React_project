21df4c339e4ea739fec299a4ba740aa1543e7978
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-07 22:55:33 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,8 +13,10 @@ let users = [
   { id: "4", name: "טליה שולץ", email: "Talia.Schultz@gmail.com", password: "!Schultz2@", role: "customer" },
   { id: "5", name: "נעמה מלכה", email: "Naama.Malka@gmail.com", password: "Naama*99", role: "manager" },
   { id: "6", name: "שירה דיין", email: "Shira@gmail.com", password: "Shira2244#", role: "customer" },
-  { id: "0656", name: "שרה כהן", email: "sara@gmail.com", password: "Ss@222222", role: "customer" },
-  { id: "9aa1", name: "אביטל הלוי 🌹", email: "A@gmail.com", password: "Aa!12345", role: "customer" }
+  { id: "7", name: "שרה כהן", email: "sara@gmail.com", password: "Ss@222222", role: "customer" },
+  { id: "8", name: "אביטל הלוי", email: "A@gmail.com", password: "Aa!12345", role: "customer" },
+  { id: "9", name: "יוסף רוב", email: "Yr@gmail.com", password: "Yyrr$111", role: "customer" },
+  { id: "10", name: "עדי חזן", email: "Hazan@gmail.com", password: "Ha#321", role: "customer" }
 ];
 
 let products = [
