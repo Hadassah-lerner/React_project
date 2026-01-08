a3a67e5ec4455475a058f342b953ebcd8386323a
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-08 19:43:46 +0200

    Update ProductCard.tsx

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,23 +1,32 @@
 import { FC } from "react";
 import { ProductModel } from "../../models/ProductModel";
 import { useNavigate } from "react-router-dom";
-import "./Product.scss";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../store/cartSlice";
+import "./ProductCard.scss";
 
-interface ProductCardProps {
+interface Props {
   product: ProductModel;
 }
 
-const ProductCard: FC<ProductCardProps> = ({ product }) => {
+const ProductCard: FC<Props> = ({ product }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   return (
-    <div
-      className="product-card"
-      onClick={() => navigate(`/products/${product.id}`)}
-    >
-      <img src={product.image} alt={product.name} />
+    <div className="product-card">
+      <img
+        src={product.image}
+        alt={product.name}
+        onClick={() => navigate(`/products/${product.id}`)}
+      />
+
       <h3>{product.name}</h3>
       <p>{product.price} ₪</p>
+
+      <button onClick={() => dispatch(addToCart(product))}>
+        הוסף לסל
+      </button>
     </div>
   );
 };
