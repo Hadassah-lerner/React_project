05c37db5a4d7e4d3b2fcc79dd60a05adf6909df4
Hadassah-lerner <hadasa.lerner@gmail.com>
2026-01-14 20:00:56 +0200

    Update axios.ts

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.60.24.105:3001',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
